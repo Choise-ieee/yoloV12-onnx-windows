c18e894c91631d0e4ca6dea0c2d3d8a3590e0065
Choise-ieee <linzhang@ieee.org>
2025-06-24 10:59:29 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+
+// Include necessary headers
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <string>
+#include "YOLO12.hpp" 
+
+#include <Windows.h>
+
+int main() {
+
+    // Configuration parameters
+    const std::string labelsPath = "coco.names";       // Path to class labels
+    const std::string modelPath = "yolov12l.onnx";     // Path to YOLO12 model
+    const std::string imagePath = "data/dog.jpg";           // Path to input image
+    bool isGPU = false;                                           // Set to false for CPU processing
+
+    // Initialize the YOLO12 detector
+    YOLO12Detector detector(modelPath, labelsPath, isGPU);
+
+    while (1)
+    {
+        
+        DWORD start_time = GetTickCount();
+
+        // Load an image
+        cv::Mat image = cv::imread(imagePath);
+
+        // Perform object detection to get bboxs
+        std::vector<Detection> detections = detector.detect(image);
+
+        // Draw bounding boxes on the image
+        detector.drawBoundingBoxMask(image, detections);
+
+        auto end = std::chrono::high_resolution_clock::now();
+
+        DWORD end_time = GetTickCount();
+        printf("The run time is:%dms\n", end_time - start_time);
+
+
+    }
+   
+
+    // Display the annotated image
+    //cv::imshow("YOLO12 Detections", image);
+    cv::waitKey(0); // Wait indefinitely until a key is pressed
+
+    return 0;
+}
+
